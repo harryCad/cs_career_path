cd0419a5767abf9066115eef8fbc82f94fad4374
Harry Cadavid <hcadavid@pxmbp1.lan>
2021-09-28 23:06:32 -0700

    finished nodes module

---
+++ data_structures_and_algorithms/nodes/2_nodes_python_getters.py
@@ -0,0 +1,23 @@
+"""
+Nodes Python Getters
+We need methods to access the data and link within the node. For this, we will use two getters, .get_value() and .get_link_node().
+
+These should each return their corresponding value on the self object.
+
+Instructions
+1.
+Implement the .get_value() getter in the Node class.
+
+2.
+Implement the .get_link_node() getter in the Node class.
+"""
+class Node:
+  def __init__(self, value, link_node=None):
+    self.value = value
+    self.link_node = link_node
+    
+  # Define your get_value and get_link_node methods below:
+  def get_value(self):
+    return self.value
+  def get_link_node(self):
+    return self.link_node
